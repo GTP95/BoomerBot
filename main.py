fe01aa89fd2d05add110c1f5539e2eaa5b977b92
Giacomo <giacomo.tom.petrucci@tiscali.it>
2023-05-09 21:44:31 +0200

    Added passing of realname from command line

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import sys
 from BoomerBot import BoomerBot
 
 if __name__ == '__main__':
-    client = BoomerBot(sys.argv[1], realname='Boomer', listOfChannels=sys.argv[2:])
+    client = BoomerBot(sys.argv[1], realname=sys.argv[2], listOfChannels=sys.argv[3:])
     client.run('raccooncity.azzurra.org', tls=True, tls_verify=False)
