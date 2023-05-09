fcd4b89840f5cb0b42eaa35db423812ca7ba6f69
Giacomo <giacomo.tom.petrucci@tiscali.it>
2023-05-09 21:40:09 +0200

    Added passing of nickname from command line

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import sys
 from BoomerBot import BoomerBot
 
 if __name__ == '__main__':
-    client = BoomerBot('Boomer', realname='Boomer', listOfChannels=sys.argv[1:])
+    client = BoomerBot(sys.argv[1], realname='Boomer', listOfChannels=sys.argv[2:])
     client.run('raccooncity.azzurra.org', tls=True, tls_verify=False)
