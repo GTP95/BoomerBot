198301738391232e81e605b96e163e98cba10d90
Giacomo <giacomo.tom.petrucci@tiscali.it>
2023-05-12 15:18:27 +0200

    Added proper argument parsing

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM python:3.9
 COPY * /root/bot/
 RUN pip install -r /root/bot/requirements.txt
 WORKDIR /root/bot
-CMD python /root/bot/main.py \#bottest
+CMD python /root/bot/main.py Boomer raccooncity.azzurra.org \#bottest
--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import sys
+import argparse
 from BoomerBot import BoomerBot
 
 if __name__ == '__main__':
-    client = BoomerBot(sys.argv[1], realname=sys.argv[2], listOfChannels=sys.argv[3:])
-    client.run('raccooncity.azzurra.org', tls=True, tls_verify=False)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('nick', help='Nickname to use', type=str)
+    parser.add_argument('-r', '--realname', help='Real name to use', type=str)
+    parser.add_argument('server', help='Server to connect to', type=str)
+    parser.add_argument('channels', nargs='+',
+                        help='Space-separated list of channels to join. If you are on Bash, remeber to escape the "#"',
+                        type=str)
+
+    args = parser.parse_args()
+
+    if not args.realname:
+        args.realname = args.nick
+
+    client = BoomerBot(args.nick, realname=args.realname, listOfChannels=args.channels)
+    client.run(args.server, tls=True, tls_verify=False)
