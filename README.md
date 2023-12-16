0457118260ac96e31bf6f6c155626f579fdd97fa
Giacomo Tommaso Petrucci <giacomo.tom.petrucci@gmail.com>
2023-12-16 21:39:20 +0100

    Updated help message and README.md. Confirmed working on Python 3.6

--- README.md
+++ README.md
@@ -1,8 +1,21 @@
 # BoomerBot
 An IRC bot to make fun of a certain other IRC bot. Based on [pydle](https://github.com/shizmob/pydle), so requires Python 3.6 through 3.9.  
 Supports NickServ authentication, no SASL yet as it isn't supported by the Azzurra IRC network.
-Usage: `python main.py <nick> <realname> <list_of_space_separated_channels>`  
+Usage: `python main.py [-h] [-s SERVER] [-p PASSWORD] nickname realname listOfChannels [listOfChannels ...]`  
+```
+positional arguments:
+  nickname              nickname to use
+  realname              realname to use
+  listOfChannels        list of channels to join
 
+optional arguments:
+  -h, --help            show help message and exit
+  -s SERVER, --server SERVER
+                        server to connect to
+  -p PASSWORD, --password PASSWORD
+                        password to use for NickServ authentication
+```
+Defaults to connecting to `raccooncity.azzurra.org` without NickServ authentication.
 
 ## Run locally
 To run it locally, first install the dependencies with `pip install -r requirements.txt`  
--- main.py
+++ main.py
@@ -6,12 +6,12 @@ import argparse
 
 if __name__ == '__main__':
     #parse command line arguments
-    parser = argparse.ArgumentParser(prog='BoomerBot', description='BoomerBot is a Python IRC bot that makes fun of a certain other bot')
+    parser = argparse.ArgumentParser(prog='main.py', description='BoomerBot is a Python IRC bot that makes fun of a certain other bot')
     parser.add_argument('nickname', type=str, help='nickname to use')
     parser.add_argument('realname', type=str, help='realname to use')
     parser.add_argument('listOfChannels', nargs='+', help='list of channels to join')
     parser.add_argument('-s', '--server', type=str, default='raccooncity.azzurra.org', help='server to connect to')
-    parser.add_argument('-p', '--password', type=str, default=None, help='password to use')
+    parser.add_argument('-p', '--password', type=str, default=None, help='password to use for NickServ authentication')
     args = parser.parse_args()
 
     client = BoomerBot(args.nickname, realname=args.realname, listOfChannels=args.listOfChannels, password=args.password)
